3e6a3a609b3868679e5a470c25d82c0a2645d0d7
Josh Sherman <josh.sherman@textus.com>
2024-09-08 16:35:20 -0600

    Update src/.chezmoi.yml Update src/.chezmoiscripts/.install-password-manager.sh Remove src/.gnupg/private-keys-v1.d/CF32DDBCBC4A96597407FD9C1C26D22039125CA0.key Remove src/.gnupg/private-keys-v1.d/D5ADB22E359EA4033BAC7EC0E27F060B598C611A.key

--- src/.chezmoiscripts/.install-password-manager.sh
+++ src/.chezmoiscripts/.install-password-manager.sh
@@ -14,19 +14,11 @@ Darwin)
     
     brew bundle install --file="/Users/jsherman/.local/share/chezmoi/src/Brewfile"
 
-    # This part if fucking stupid, but it is what it is right now....
-    # Dashlane does NOT have CLI support for logging in without user 
-    # interation. There is no silent mode or -y or anything. So when
-    # the `sync` command runs, you'll have to copy the URL, open
-    # something to use to hit that URL and do that, and then use the 
-    # arrow keys to select the "email" option. Then you wait, copy the
-    # code from the email, and manually paste it here. 🤦
     dcli sync
     dcli n DOTFILES_PRIVATE_KEY | ssh-add -
 
-    # if we can't brew
-    # curl https://github.com/Dashlane/dashlane-cli/releases/download/v6.2436.0/dcli-macos-arm64 "/usr/local/bin"
-    # chmod +x dcli-macos-arm64
+    # Handling the upload of gpg keys here as well, so that they are available as soon as possible.
+    gpg --import ~/.local/share/chezmoi/src/.keys/dotfiles.asc
 
     ;;
 Linux)
