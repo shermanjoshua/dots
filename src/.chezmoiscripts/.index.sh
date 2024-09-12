4c1dbe8831a8ee0e589a087db4a2623560e227f3
Josh Shermanhint = 'floating-big-letter'hint = 'floating-big-letterj <josh.sherman@textus.com>
2024-09-12 10:13:19 -0600

    ssh working

--- src/.chezmoiscripts/.index.sh
+++ src/.chezmoiscripts/.index.sh
@@ -3,9 +3,8 @@
 set -eu
 
 declare script_dir=$PWD/$(dirname "$0")
-declare -a file_list=()
 
-for file in $(find "$script_dir" -type f -name '.*'); do
+for file in $(find "$script_dir" -type f -name ".*"); do
   if [[ ! "$file" == *"index.sh" ]]; then
     /bin/bash $file
   fi
